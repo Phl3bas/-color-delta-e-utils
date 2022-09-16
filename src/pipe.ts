401aabaeb64c3192b7f62d2701cac0ac5a0eaa80
phl3bas <phl3bas87@gmail.com>
2022-09-16 11:48:22 +0100

    test: pipe tests

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,4 +1,4 @@
-export function pipe(...funcs: Array<(...args: any[]) => any>): (...args: any[]) => any[] {
+export function pipe(...funcs: Array<(...args: any[]) => any>): (base: string, startingList: any[], options?: Record<string, any>) => any[] {
   return (base: string, startingList: any[], options?: Record<string, any>) => {
     return funcs.reduce((acc, cur) => {
       return cur(base, acc, options)
--- test/pipe.test.ts
+++ test/pipe.test.ts
@@ -1,11 +1,67 @@
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { isFunction } from 'yewtils'
+import { pipe } from '../src'
 
+const mockFn = vi.fn()
+const mockFn2 = vi.fn()
 
-describe('pipe', ()=>{
+mockFn.mockImplementation((_, arr) => arr)
+mockFn2.mockImplementation((c: string, arr: any[]): any[] => {
+  const newArr = arr.slice(1)
 
-    it('dummy test', ()=>{
+  return newArr
+})
 
-        expect(true).toBe(true)
+describe('pipe', () => {
+  beforeEach(() => {
+    mockFn.mockClear()
+    mockFn2.mockClear()
+  })
 
-    })
+  it('will return a function', () => {
+    const testFn = pipe()
+
+    expect(isFunction(testFn)).toBe(true)
+  })
+
+  it('will return a function that calls piped functions', () => {
+    const testFn = pipe(mockFn)
+
+    testFn('#000', ['#fff'])
+
+    expect(mockFn.mock.calls.length).toBe(1)
+  })
+
+  it('will pass params to piped functions', () => {
+    const testFn = pipe(mockFn)
+
+    testFn('#000', ['#fff'])
+
+    expect(mockFn).toBeCalledWith('#000', ['#fff'], undefined)
+  })
+
+  it('will pass result of previous function to param of next piped function', () => {
+    const testFn = pipe(mockFn, mockFn2)
+    testFn('#000', ['#fff', '#222'])
+    expect(mockFn).toBeCalledWith('#000', ['#fff', '#222'], undefined)
+    expect(mockFn2).toBeCalledWith('#000', ['#fff', '#222'], undefined)
+  })
+
+  it('will pass the comparitor to all function', () => {
+    const comparitor = '#000'
+
+    const testFn = pipe(mockFn, mockFn2)
+    testFn(comparitor, ['#fff', '#222'])
+    expect(mockFn.mock.calls[0][0]).toBe(comparitor)
+    expect(mockFn2.mock.calls[0][0]).toBe(comparitor)
+  })
+
+  it('will pass the options to all function', () => {
+    const options = { test: true }
+
+    const testFn = pipe(mockFn, mockFn2)
+    testFn('#000', ['#fff', '#222'], options)
+    expect(mockFn.mock.calls[0][2]).toMatchObject(options)
+    expect(mockFn2.mock.calls[0][2]).toMatchObject(options)
+  })
 })
